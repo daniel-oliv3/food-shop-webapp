a975369741cf3dd25f06a967f8b297ee3842cafc
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-11 10:46:24 -0300

    Menu section

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 ### Food Shop
 
 ## Comandos para a criação do projeto.
-- Component Navbar - barra de navegação
+- Component Navbar - Barra de navegação
 - ng g c navbar
-
 
 - Component Footer - Rodapé
 - ng g c footer
+
+- Component Menu - Menu
+- ng g c menu
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './sharepage/navbar/navbar.component';
 import { FooterComponent } from './sharepage/footer/footer.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
