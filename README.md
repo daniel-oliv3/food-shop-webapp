482c8afeea5fbcfaf68d765bc5be6b051fc5b609
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-11 11:09:17 -0300

    Update

--- README.md
+++ README.md
@@ -19,4 +19,17 @@
 - Component Pages Contact - Contato
 - ng g c contact
 
+## Routes
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'menu', component:MenuComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'contact', component:ContactComponent}
+];
+
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'menu', component:MenuComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'contact', component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
