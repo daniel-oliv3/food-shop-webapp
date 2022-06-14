379bc2c6bb6be158ad3ae21fa3df867f3cb68f29
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-14 16:56:01 -0300

    Up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +19,7 @@ import { ContactComponent } from './pages/contact/contact.component';
     MenuComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/services/order-details.service.ts
@@ -0,0 +1,53 @@
+export class OrderDetailsServices {
+
+  constructor(){
+
+  }
+
+
+  foodDetails = [
+    {
+      id: 1,
+      foodName: "Sanduiche natural de frango",
+      foodDetails: "Sanduiche natural de frango",
+      foodPrice: 200,
+      foodImg: "https://images.pexels.com/photos/5967861/pexels-photo-5967861.jpeg",
+    },
+    {
+      id: 2,
+      foodName: "Pizza de Mussarela e Bacon",
+      foodDetails: "Cebela tomate mussarela bacon",
+      foodPrice: 369,
+      foodImg: "https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/216054.jpg",
+    },
+    {
+      id: 3,
+      foodName: "Hamburguer",
+      foodDetails: "O mais gostoso",
+      foodPrice: 149,
+      foodImg: "https://images.pexels.com/photos/1633578/pexels-photo-1633578.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 4,
+      foodName: "Hot Dog com Mel",
+      foodDetails: "Kikão delicioso",
+      foodPrice: 200,
+      foodImg: "https://images.pexels.com/photos/4518656/pexels-photo-4518656.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 5,
+      foodName: "Ramen",
+      foodDetails: "Ramen delicioso",
+      foodPrice: 369,
+      foodImg: "https://images.pexels.com/photos/2591594/pexels-photo-2591594.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+    {
+      id: 6,
+      foodName: "Sushi Japones",
+      foodDetails: "O mais gostoso",
+      foodPrice: 149,
+      foodImg: "https://images.pexels.com/photos/2323398/pexels-photo-2323398.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    }
+  ]
+
+}
